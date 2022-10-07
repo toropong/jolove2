1cf46d7194c021028850f0112f1f1409fee1230f
JungKyungJin <rudwls512@naver.com>
2022-10-07 16:01:29 +0900

    image file upload

--- laravel/app/Http/Controllers/WorkController.php
+++ laravel/app/Http/Controllers/WorkController.php
@@ -2,14 +2,13 @@
 
 namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
-use App\Models\Works;
+// use App\Models\Works;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Config;
 use Carbon\Carbon;
-use Session;
 
 
 class WorkController extends Controller
@@ -22,14 +21,12 @@ class WorkController extends Controller
 
     public function store(Request $request)
     {
-        
-        $files = $request -> file('images');
+     
         // $files = $request->file('picture');
         $path = $request -> file('picture') -> store('/', 'public');
-        $subpath =  $files[0] -> store('/', 'public');
-        $subpath2 =  $files[0] -> store('/', 'public');
-        console.log("$file[0],$file[1]");
-        // $subpath3 = $request -> file('images') -> store('/', 'public');
+        $subpath =  $request -> file('picture2') -> store('/', 'public');
+        $subpath2 =  $request -> file('picture3') -> store('/', 'public');
+        $subpath3 = $request -> file('picture4') -> store('/', 'public');
         // $subpath4 = $request -> file('images') -> store('/', 'public');
         // $subpath5 = $request -> file('images') -> store('/', 'public');
 
@@ -41,52 +38,28 @@ class WorkController extends Controller
         'filename'=>$path,
         'subimage_1'=>$subpath,
         'subimage_2'=>$subpath2,
-        // 'subimage_3'=>$subpath3,
+        'subimage_3'=>$subpath3,
         // 'subimage_4'=>$subpath4,
         // 'subimage_5'=>$subpath5
         ]);
         return redirect('/');
     
-           
+            
     }
 
     public function index(Request $request)
     {
     
-
         $works=DB::table('works')->select('*') -> orderby("no", "desc")->paginate(10);
-
-
     
         return view('/index', compact('works')
         );
     }
 
     public function product(Request $request){
-        $id = session()->get('userid');
-        //조회수 카운트
-        if( $visit =\App\Models\Works::where('no', $request->no)
-        ->first() ) {
-            
-            $visit->visit_count++;
-            $data["result"] = $visit->update();
-            $data["no"] = $visit->no;
-        } else {
-            $visit = new \App\Models\Works;
-            $visit->no = $request->no;
-            $visit->visit_count++;
-            $data["result"] = $visit->save();
-            $data["no"] = $visit->no;
-        }         
 
-        $data["product"] = [];
-        $data["product"]= \App\Models\Works::select()
-        ->where(function ($query) use ($request) {
-            if ($request->no) {
-                    $query->where("no", $request->no);
-            }         
-        })->first();
-        // $count = DB::table('works')->select('visit_count')->where(['no',$request->no])->get();
-        return view('product', $data);
+        $product=DB::table('works')->select('*')->get();
+        return view('product', compact('product')
+        );
     }
 }
--- laravel/resources/views/image.blade.php
+++ laravel/resources/views/image.blade.php
@@ -44,18 +44,15 @@
           
           {{-- @endforeach --}}
     <input type="file" onchange="checkFile(this);" multiple id="real-input" name="picture" class="image_inputType_file" accept="image*/">
-    
+   
+    <input type="file"  multiple id="real-input" name="picture2" class="image_inputType_file" accept="image*/">
+   
+    <input type="file"  multiple id="real-input" name="picture3" class="image_inputType_file" accept="image*/">
+   
+    <input type="file"  multiple id="real-input" name="picture4" class="image_inputType_file" accept="image*/">
+   
     {{-- <form method="POST" onsubmit="return false;" enctype="multipart/form-data"> --}}
-    <div class="insert">
-      <input type="file" onchange="addFile(this);" name="images[]" multiple='multiple'  id="subfile" />
-          {{-- <label for="file">
-            <div class="btn-upload">추가이미지</div>
-          </label> --}}
-          
-          <div class="file-list">
-            
-          </div>
-  </div>
+
 
 
     <div class="c">
@@ -152,114 +149,4 @@
                           var filesArr = new Array();
 
 /* 첨부파일 추가 */
-function addFile(obj){
 
-    var maxFileCnt = 5;   // 첨부파일 최대 개수
-    var attFileCnt = document.querySelectorAll('.filebox').length;    // 기존 추가된 첨부파일 개수
-    var remainFileCnt = maxFileCnt - attFileCnt;    // 추가로 첨부가능한 개수
-    var curFileCnt = obj.files.length;  // 현재 선택된 첨부파일 개수
-    // var file = document.getElementById("subfile");
-   
-    // for(var i = 0; i<maxFileCnt; i++){
-    //   $('input[name="images[i]"].'.file)
-    // }
-    // 첨부파일 개수 확인
-    if (curFileCnt > remainFileCnt) {
-        alert("첨부파일은 최대 " + maxFileCnt + "개 까지 첨부 가능합니다.");
-    }
-
-    for (var i = 0; i < Math.min(curFileCnt, remainFileCnt); i++) {
-
-        var file = obj.files[i];
-
-        // 첨부파일 검증
-        if (validation(file)) {
-            // 파일 배열에 담기
-            var reader = new FileReader();
-            reader.onload = function () {
-                filesArr.push(file);
-            };
-            reader.readAsDataURL(file)
-
-            // 목록 추가
-            let htmlData = '';
-            // htmlData += '<input type="hidden" name = "file' + fileNo +'" + id= "file'+fileNo">';
-            htmlData += '<div id="file' + fileNo + '" name="file' + fileNo+ '"  class="filebox" >';
-            htmlData += '   <p class="name">' + file.name + '</p>';
-            htmlData += '   <a class="delete" onclick="deleteFile(' + fileNo + ');"><i class="far fa-minus-square"></i></a>';
-            htmlData += '</div>';
-            
-            $('.file-list').append(htmlData);
-            fileNo++;
-        } else {
-            continue;
-        }
-
-    }
-
-
-    console.log(file);
-console.log(filesArr);
-console.log(reader);
-console.log(fileNo);
-
-// console.log(filesArr[num]);
-
-    // 초기화
-    // document.querySelector("input[type=file]").value = "";
-}
-
-/* 첨부파일 검증 */
-function validation(obj){
-    const fileTypes = ['image/gif', 'image/jpeg', 'image/png', 'image/bmp', 'image/tif'];
-    if (obj.name.length > 100) {
-        alert("파일명이 100자 이상인 파일은 제외되었습니다.");
-        return false;
-    } else if (obj.size > (100 * 1024 * 1024)) {
-        alert("최대 파일 용량인 100MB를 초과한 파일은 제외되었습니다.");
-        return false;
-    } else if (obj.name.lastIndexOf('.') == -1) {
-        alert("확장자가 없는 파일은 제외되었습니다.");
-        return false;
-    } else if (!fileTypes.includes(obj.type)) {
-        alert("이미지 파일만 등록 가능합니다.");
-        return false;
-    } else {
-        return true;
-    }
-}
-function submitForm() {
-    // 폼데이터 담기
-    var form = document.querySelector("input");
-    var formData = new FormData(form);
-    for (var i = 0; i < filesArr.length; i++) {
-        // 삭제되지 않은 파일만 폼데이터에 담기
-        if (!filesArr[i].is_delete) {
-            formData.append("images", filesArr[i]);
-        }
-    }
-    $.ajax({
-        method: 'POST',
-        url: '/image',
-        dataType: 'json',
-        data: formData,
-        async: true,
-        timeout: 30000,
-        cache: false,
-        headers: {'cache-control': 'no-cache', 'pragma': 'no-cache'},
-        success: function () {
-            alert("파일업로드 성공");
-        },
-        error: function (xhr, desc, err) {
-            alert('에러가 발생 하였습니다.');
-            return;
-        }
-    })
-  }
-
-/* 첨부파일 삭제 */
-function deleteFile(num) {
-    document.querySelector("#file" + num).remove();
-    filesArr[num].is_delete = true;
-}
-                        </script>
