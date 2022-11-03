b9dd7d5fe6e29bcfe491129f63753a99355b7d80
toropong <leemk2448@naver.com>
2022-11-03 10:24:53 +0900

    수정완료

--- laravel/app/Models/Comments.php
+++ laravel/app/Models/Comments.php
@@ -5,11 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Likes extends Model
+class Comments extends Model
 {
     use HasFactory;
 
     protected $primaryKey = 'c_no';
     public $incrementing = true;
 
-}
+}   
--- laravel/resources/views/product.blade.php
+++ laravel/resources/views/product.blade.php
@@ -5,9 +5,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
-        <meta name="csrf-token" content="{{ csrf_token() }}" />
+        <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>상세페이지</title>
-        
         <!-- Favicon-->
         <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
         <!-- Core theme CSS (includes Bootstrap)-->
@@ -30,16 +29,13 @@
                 <div class="col-lg-5">
                     <h1 class="font-weight-light"></h1>
                     <p>{{$products['title']}}</p>
-                    <a class="btn btn-primary" href="#!">♥소스코드 보기</a>
+                    <a class="btn btn-primary" href="#!">소스코드 보기</a>
                 </div>
             </div>
             @endforeach
             @auth
-            {{-- @foreach($favorite as $favorites) --}}
             <th>좋아요</th>
-<td> <input id="likebtn" type="button" value="♡" style="border: none"/>좋아요갯수</td>
-            {{$likes}}개
-{{-- @endforeach --}}
+<td> <input id="likebtn" type="button" value="♥" /></td>
             @else
             <th>좋아요</th>
             <td> <input type="button" value="♥" style="pointer-events: none"/> </td> 
@@ -102,7 +98,51 @@
                 </div>
             </div>
             
-        </div>
+
+
+            <div class="people_comment">
+            <div class="dat_lab">
+              <h2>댓글</h2>
+            </div>
+            <form class="form-horizontal" role="form" name="frm_custom" id="frm_custom">
+            {{csrf_field()}}
+            @foreach($comment as $comments)
+              <input type="hidden" name="no" id="no" value="{{ $comments['w_no'] }}">
+            @endforeach
+              <div class="combox">
+                <div class="comcontent">
+                  <div class="comment_munie">
+                    <textarea class="comment_text" name="c_comments" id="comment_texts" rows="8" cols="80" placeholder="댓글 내용 입력" required></textarea>
+                  </div>
+                  <div class="col-xs-12 mt-3 text-center">
+                        <button type="button" class="btn btn-sm btn-primary" id="btn_custom_update">글작성</button>
+                    </div>
+                </div>
+                </div>
+              </form>
+              <div class="comment_new">
+                <div class="">
+
+                  <h3>전체 댓글</h3>
+                </div>
+                @foreach ($comment as $key => $comments)
+                  <div class="create_comment">
+                    <div class="neadcomt">
+                      <div class="comment_naeyoung">
+                        <div class="comment_people">
+
+                          {{$comments['u_no']}}
+                        </div>
+                        <div class="value_comment">
+                          <p> {{$comments['c_comments']}}</p>
+                        </div>
+                    </div>
+                  </div>
+                @endforeach
+              </div>
+            </div>
+          </div>
+
     </section>
         <!-- Footer-->
         <footer class="bg-dark py-4 mt-auto">
@@ -123,12 +163,58 @@
         </footer>
         
                 
-    <script type="text/javascript">
+    <script>
+  
         $(document).ready(function(){
             $("#likebtn").click(likedata);
+            console.log("이거까진됨");
         });
+        function likedata(){
+            $.ajax({
+                url:'/product/like/{no}',
+                type: 'post',
+                dataType: 'json',
+                data: {"likevalue": $("#likebtn").val()},
+                
+                success: function(data, statusText, jqXHR){
 
         var w_no = {{$products->no}};
+
+
+        $(document).on("click", "#btn_custom_update", function () {
+                custom_update();
+            });
+
+    function custom_update() {
+        var req = $("#frm_custom").serialize();
+        $.ajax({
+            headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
+            url: '/comment/update',
+            type: 'POST',
+            async: true,
+            beforeSend: function (xhr) {
+                $("#customDetail_msg").html("");
+            },
+            data: req,
+            success: function (res, textStatus, xhr) {
+                if (res.result == true) {
+                    document.location.reload();
+                } else {
+                    $("#customDetail_msg").html(res.message);
+                    console.log(xhr);
+                    console.log(textStatus);
+                    console.log(errorThrown);
+                }
+            },
+            error: function (xhr, textStatus, errorThrown) {
+                console.log(xhr);
+                console.log(textStatus);
+                console.log(errorThrown);
+            }
+        });
+}
+
 
 function likedata(){
   $.ajax({
@@ -146,12 +232,6 @@ function likedata(){
                     console.log(data); //응답 body부 데이터
                     	console.log(statusText); //"succes"로 고정인듯함
                     	console.log(jqXHR);
-                        if(data=="1"){
-                            $("#likebtn").attr("value","♥");
-                        }
-                        else{
-                            $("#likebtn").attr("value","♡");
-                        }
                         
                 },
                 error: function(jqXHR, textStatus, errorThrown){
@@ -159,15 +239,16 @@ function likedata(){
                     	console.log(textStatus); //"error"로 고정인듯함
                     	console.log(errorThrown);
                     console.log("실패");
+                }
+            })
+        }
+        $.ajaxSetup({
+  headers: {
+    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
   }
-})
-}
- 
-
+});
     </script>
         <!-- Bootstrap core JS-->
-        
-        <script src="https://code.jquery.com/jquery-latest.min.js"></script> 
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
         <!-- Core theme JS-->
         {{-- <script src="/js/scripts.js"></script> --}}
