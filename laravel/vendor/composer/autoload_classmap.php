5a98c0d9efde9f087fb9c088806b3b689e84f4f9
toropong <leemk2448@naver.com>
2022-11-03 10:05:18 +0900

    테이블

--- laravel/vendor/composer/autoload_classmap.php
+++ laravel/vendor/composer/autoload_classmap.php
@@ -29,6 +29,7 @@ return array(
     'App\\Http\\Middleware\\TrustProxies' => $baseDir . '/app/Http/Middleware/TrustProxies.php',
     'App\\Http\\Middleware\\VerifyCsrfToken' => $baseDir . '/app/Http/Middleware/VerifyCsrfToken.php',
     'App\\Models\\Customer' => $baseDir . '/app/Models/Customer.php',
+    'App\\Models\\Likes' => $baseDir . '/app/Models/Likes.php',
     'App\\Models\\User' => $baseDir . '/app/Models/User.php',
     'App\\Models\\Works' => $baseDir . '/app/Models/Works.php',
     'App\\Providers\\AppServiceProvider' => $baseDir . '/app/Providers/AppServiceProvider.php',
