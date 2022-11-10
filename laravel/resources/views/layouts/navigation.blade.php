370550c45d086ddb33093c8b44a8aa6c67f068ce
toropong <leemk2448@naver.com>
2022-11-11 01:46:27 +0900

    라우트 변경     /pubic <- xxx  / <-ooo

--- laravel/resources/views/layouts/navigation.blade.php
+++ laravel/resources/views/layouts/navigation.blade.php
@@ -6,12 +6,12 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                 <li class="nav-item"><a class="nav-link" href="/">홈</a></li>
-                <li class="nav-item"><a class="nav-link" href="/public/index">작품 소개</a></li>
+                <li class="nav-item"><a class="nav-link" href="/index">작품 소개</a></li>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" id="navbarDropdownBlog" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">작품 리스트</a>
                     <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownBlog">
-                        <li><a class="dropdown-item" href="/public/index/2021">2021 학년도</a></li>
-                        <li><a class="dropdown-item" href="/public/index/2022">2022 학년도</a></li>
+                        <li><a class="dropdown-item" href="/index/2021">2021 학년도</a></li>
+                        <li><a class="dropdown-item" href="/index/2022">2022 학년도</a></li>
                                             </ul>
                                         </li>
                                         @auth
@@ -36,7 +36,7 @@
                         <script src="//code.jquery.com/jquery-3.3.1.min.js"></script>
                         <script type="text/javascript">
                         function showPopup() {
-  var url="/public/image_popup";
+  var url="/image_popup";
   var option="width=700, height=300, top=200"
   window.open(url, "", option);
 }
--- laravel/storage/framework/views/932bac0988f435133e3f98979abf8287db47236a.php
+++ laravel/storage/framework/views/932bac0988f435133e3f98979abf8287db47236a.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <meta name="description" content="" />
     <meta name="author" content="" />
-    <title>Modern Business - Start Bootstrap Template</title>
+    <title>중부대학교 졸업 작품 공유 사이트</title>
     <!-- Favicon-->
     <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
     <!-- Bootstrap icons-->
