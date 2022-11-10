e6523b72236e8446b832c536d94975309a112e9a
toropong <leemk2448@naver.com>
2022-11-11 00:00:59 +0900

    깃로그

---
+++ laravel/storage/framework/views/056f09977d889ea83ce3557f6c18f23ca4a9be64.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <meta name="csrf-token" content="<?php echo e(csrf_token()); ?>" />
+        <title>Shop Homepage - Start Bootstrap Template</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Bootstrap icons-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        
+    </head>
+    <body>
+        <!-- Navigation-->
+<?php echo $__env->make('layouts.navigation', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+        <!-- Header-->
+        <header class="bg-dark py-5">
+            <div class="container px-4 px-lg-5 my-5">
+                <div class="text-center text-white">
+                    <h1 class="display-4 fw-bolder">졸업 작품</h1>
+                    <!-- <p class="lead fw-normal text-white-50 mb-0">With this shop hompeage template</p> -->
+                </div>
+            </div>
+        </header>
+        <!-- Section-->
+        <section class="py-5">
+            <form action="<?php echo e(url('search')); ?>" method="post" style="text-align: center" >
+                           <?php echo e(csrf_field()); ?>
+
+                <input type="text" name="query" maxlength="255"
+                autocomplete="off" >
+                <input type="submit" value="검색">
+            </form>
+            <div class="container px-4 px-lg-5 mt-5" id= first-display>
+                <button onclick="showdisplay()" style="margin-bottom: 10px">리스트변경</button>
+                <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
+                <?php if(isset($lists)): ?>
+                <?php $__currentLoopData = $lists; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $list): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                
+                    <div class="big-container" style="width: 20%; height: 20%; border:1px solid; margin:2%; border-radius:3px; display:inline-block ">
+                        <!-- Product image-->
+                        <div class="image-container" style="width: 100%; height:100%; display: inline-block  ">
+                            <div class="image-small" style="width: 100%; height:100%; display:inline-block; position:relative">
+                        <img src="\storage\app\public\<?php echo e($list['filename']); ?>" style="width:100%; height:100%; object-fit: cover;" >
+                        </div>    
+                        </div>
+                        <div class="medium-container">
+                                <div class="text-center">
+                                    <h5 class="fw-bolder"><?php echo e($list['title']); ?></h5>
+                                </div>
+                            </div>
+                            <div class="small-container">
+                                <div class="text-center">
+                                    <a class="btn btn-outline-dark mt-auto" href="/public/product/<?php echo e($list['no']); ?>" >
+                                        작품 보기
+                                    </a>
+                                    <?php if(auth()->guard()->check()): ?>
+                                    <a class="btn btn-outline-dark mt-auto" href="">작품 삭제</a>
+                                    <?php endif; ?>
+                                </div>
+                            </div>
+                        </div>
+                    
+                    <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+
+<?php endif; ?>      
+
+                </div>
+                <div style="text-align: center; margin-top:50px;"> 
+                <?php echo e($lists->links()); ?>         
+                </div>
+            </div>
+            
+        </section>
+        <div class="container px-4 px-lg-5 mt-5" >
+            
+            <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
+                
+<div class="container_2" style="width: 80%; display:none; margin-bottom:20px;" id="second-display" >
+    <button onclick="showdisplay2()">리스트변경</button>
+    <h3><code>.졸업작품</code></h3> 
+    <?php if(isset($lists)): ?>
+    <?php $__currentLoopData = $lists; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $list): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>           
+    <table class="table">
+      <thead>
+        <tr>
+          <th>번호</th>
+          <th>작품이름</th>
+          <th>작품설명</th>
+        </tr>
+      </thead>
+     
+      <tbody>
+        <tr>
+          <td><?php echo e($list['no']); ?></td>
+          <td><a href="/public/product/<?php echo e($list['no']); ?>"><?php echo e($list['title']); ?></a></td>
+          <td><a href="/public/product/<?php echo e($list['no']); ?>"><?php echo e($list['cont']); ?></a></td>
+        </tr>
+      </tbody>
+    </table>
+    <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+    <?php endif; ?>
+    <div style="text-align: center; margin-top:50px;"> 
+        <?php echo e($lists->links()); ?>         
+        </div>
+  </div>  
+       
+    </div>
+        </div>
+        
+        <!-- Footer-->
+        <footer class="py-5 bg-dark">
+            <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Your Website 2022</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        
+        <script>
+            function showdisplay(){
+                $('#first-display').hide();
+                $('#second-display').show();
+            }
+            function showdisplay2(){
+                $('#second-display').hide();
+                $('#first-display').show();
+            }
+        </script>
+        
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        
+    </body>
+</html><?php /**PATH C:\xampp\htdocs\jolove2\laravel\resources\views/index.blade.php ENDPATH**/ ?>
---
+++ laravel/storage/framework/views/e16f9a49adeb8830092172c148449490a9883970.php
@@ -0,0 +1,306 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <meta name="csrf-token" content="<?php echo e(csrf_token()); ?>">
+        <title>상세페이지</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+    </head>
+    <body>
+        <!-- Responsive navbar-->
+        <?php echo $__env->make('layouts.navigation', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+        <!-- Page Content-->
+        <section class="py-5">
+        <div class="container px-4 px-lg-5" style="width: 100%; height:100%;">
+            <h2 class="title-image" style="margin-top: 1rem;">대표이미지</h2>
+            <div class="row gx-4 gx-lg-5 align-items-center my-5" style="width: 100%">
+                <?php if(isset($product)): ?>
+            <?php $__currentLoopData = $product; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $products): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                
+                <div class="col-lg-7"><img class="img-fluid rounded mb-4 mb-lg-0" src="\storage\app\public\<?php echo e($products['filename']); ?>" width="200" height="200" style="border-style: solid" onerror="this.style.display='none'" /></div>
+                <div class="col-lg-5">
+                    <h1 class="font-weight-light"></h1>
+                    <p><?php echo e($products['title']); ?></p>
+                    <a class="btn btn-primary" href="#!">소스코드 보기</a>
+                </div>
+            </div>
+            <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+            <?php if( isset ( Auth::user()->id) ): ?> 
+            
+            <th>좋아요</th>
+            <?php if($likes2==1): ?>
+<td> <input id="likebtn" type="button" value="♥" /><input id = "count"  style="pointer-events: none" value="<?php echo e($likes); ?>"></td>
+            <?php else: ?>
+                <td> <input id="likebtn" type="button" value="♡" /><input id = "count"  style="pointer-events: none" value="<?php echo e($likes); ?>"></td>
+            <?php endif; ?>
+            <?php else: ?>
+            <th>좋아요</th>
+            <td> <input type="button" value="♥" style="pointer-events: none"/><input id = "count" value="<?php echo e($likes); ?>"></td> 
+            <?php endif; ?>
+            <?php endif; ?> 
+           
+            
+
+    
+               <div class="view isk">
+                    <img src="/img/eye.png" width="16" height="16" alt="조회수">
+                    <div class="see_num intf" name="">
+                     <?php if($products['visit_count'] !=0): ?>
+                        <span><?php echo e($products['visit_count']); ?></span>
+                      <?php else: ?>
+                        <span>0</span>
+                      <?php endif; ?> 
+            </div>
+        
+            
+            <!-- Call to Action-->
+            
+   
+            <!-- Content Row-->
+           
+            <div class="row gx-4 gx-lg-5">
+                <div class="col-md-4 mb-5">
+                    <div class="card h-100">
+                        <div class="card-body">
+                            <h2 class="card-title">이미지 2</h2>
+                            <?php $__currentLoopData = $product; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $products): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                           <img class="img-fluid rounded mb-4 mb-lg-0" src= "\storage\app\public\<?php echo e($products['subimage_1']); ?>" width="200" height="200" onerror="this.style.display='none'" />
+                        <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                        </div>
+                        <div class="card-footer"><a class="btn btn-primary btn-sm" href="#!">보러가기</a></div>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-5">
+                    <div class="card h-100">
+                        <div class="card-body">
+                            <h2 class="card-title">이미지 3</h2>
+                            <?php $__currentLoopData = $product; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $products): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                            <img class="img-fluid rounded mb-4 mb-lg-0" src= "\storage\app\public\<?php echo e($products['subimage_2']); ?>" width="200" height="200"onerror="this.style.display='none'"/>
+                            <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                        </div>
+                        <div class="card-footer"><a class="btn btn-primary btn-sm" href="#!">보러가기</a></div>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-5">
+                    <div class="card h-100">
+                        <div class="card-body">
+                            <h2 class="card-title">이미지 4</h2>
+                            <?php $__currentLoopData = $product; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $products): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                            <img class="img-fluid rounded mb-4 mb-lg-0"src= "\storage\app\public\<?php echo e($products['subimage_3']); ?>" width="200" height="200" onerror="this.style.display='none'"/>
+                            <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                        </div>
+                        <div class="card-footer"><a class="btn btn-primary btn-sm" href="#!">보러가기</a></div>
+                    </div>
+                </div>
+            </div>
+            
+
+
+            <div class="people_comment">
+            <div class="dat_lab">
+              <h2>댓글</h2>
+            </div>
+
+            <form class="form-horizontal" method="POST"  role="form" name="frm_custom" id="frm_custom"  enctype="multipart/form-data">
+                <?php echo e(csrf_field()); ?>
+
+                <?php $__currentLoopData = $comment; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $comments): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+              <input type="hidden" name="no" id="no" value="<?php echo e($comments['w_no']); ?>"> 
+              <input type="hidden" name="c_no" id="c_no" value="<?php echo e($comments['c_no']); ?>"> 
+                 <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+              <div class="combox">
+                <div class="comcontent">
+                  <div class="comment_munie">
+                    <textarea class="comment_text" name="c_comments" id="c_comments" rows="8" cols="80" placeholder="댓글 내용 입력" required></textarea>
+                  </div>
+                    <div class="col-xs-12 mt-3 text-center">
+                        <button type="button" class="btn btn-sm btn-primary" id="btn_custom_update">글작성</button>
+                    </div>
+                </div>
+                </div>
+            </form>
+
+
+              <div class="comment_new">
+                <div class="">
+
+                  <h3>전체 댓글</h3>
+                </div>
+                     <?php $__currentLoopData = $comment; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $key => $comments): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                  <div class="create_comment">
+                    <div class="neadcomt">
+                      <div class="comment_naeyoung">
+                        <div class="comment_people">
+
+                          <?php echo e($comments['u_no']); ?>
+
+                        </div>
+                        <div class="value_comment">
+                          <p> <?php echo e($comments['c_comments']); ?></p>
+                        </div>
+                    </div>
+
+                    <div class="delete">
+                    <button type="button" class="btn btn-sm btn-primary" id="btn_comment_delete">글삭제</button>
+                    </div>
+
+                  </div>
+                <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+              </div>
+            </div>
+          </div>
+
+    </section>
+        <!-- Footer-->
+        <footer class="bg-dark py-4 mt-auto">
+            <div class="container px-5">
+                <div class="row align-items-center justify-content-between flex-column flex-sm-row">
+                    <div class="col-auto">
+                        <div class="small m-0 text-white">Copyright &copy; Your Website 2022</div>
+                    </div>
+                    <div class="col-auto">
+                        <a class="link-light small" href="/manage/register">등록하기</a>
+                        <span class="text-white mx-1">&middot;</span>
+                        <a class="link-light small" href="#!">Terms</a>
+                        <span class="text-white mx-1">&middot;</span>
+                        <a class="link-light small" href="#!">Contact</a>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        
+
+
+        <script type="text/javascript">
+            
+       
+        $(document).on("click", "#btn_custom_update", function () {
+                custom_update();
+            });
+
+        $(document).on("click", "#btn_comment_delete", function () {
+            comment_delete();
+        });
+
+        $(document).ready(function(){
+            $("#likebtn").click(likedata);
+        });
+
+        
+
+        var w_no = <?php echo e($products->no); ?>;
+        var c_comments = $("#c_comments").serialize();
+        var c_no = $("#c_no").serialize();
+
+
+    function likedata(){
+    $.ajax({
+        type : 'POST',
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
+        url : 'test',
+        async : true,
+    
+            dataType : 'json',
+            data : {"likevalue" : $("#likebtn").val(), 
+            "w_no" : w_no},
+            success: function(data, statusText, jqXHR){
+                        console.log("성공")
+                        console.log(data); //응답 body부 데이터
+                        console.log(statusText); //"succes"로 고정인듯함
+                        console.log(jqXHR);
+                            if(data['a']=="1"){
+                                $("#likebtn").attr("value","♥");
+                                $("#count").attr("value",data['b']);
+                            }
+                            else{
+                                $("#likebtn").attr("value","♡");
+                                $("#count").attr("value",data['b']);
+                            }
+                           
+                            
+                    },
+                    error: function(jqXHR, textStatus, errorThrown){
+                        console.log(jqXHR);  //응답 메시지
+                        console.log(textStatus); //"error"로 고정인듯함
+                        console.log(errorThrown);
+                        console.log("실패");
+                    }
+                })
+}
+
+    function custom_update() {
+
+        $.ajax({
+    type : 'POST',
+    headers: {
+        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
+    url : 'update', 
+    async : true,
+   data: {'c_comments' : c_comments,
+            'w_no' : w_no},
+         dataType : 'json',
+         success: function(data, statusText, jqXHR){
+                    console.log("성공")
+                    console.log(data); //응답 body부 데이터
+                       console.log(statusText); //"succes"로 고정인듯함
+                       console.log(jqXHR);
+                },
+                
+                error: function(jqXHR, textStatus, errorThrown){
+                    console.log(jqXHR);  //응답 메시지
+                       console.log(textStatus); //"error"로 고정인듯함
+                       console.log(errorThrown);
+                    console.log("실패");
+                 }
+            })
+    }
+
+
+    function comment_delete() {
+    $.ajax({
+    type : 'POST',
+    headers: {
+    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
+    url : 'delete', 
+    async : true,
+    data: {
+        'c_no' :c_no,
+        'c_comments' : c_comments,
+        'w_no' : w_no},
+    dataType : 'json',
+    success: function(data, statusText, jqXHR){
+                console.log("성공")
+                console.log(data); //응답 body부 데이터
+                console.log(statusText); //"succes"로 고정인듯함
+                console.log(jqXHR);
+            },
+            
+            error: function(jqXHR, textStatus, errorThrown){
+                console.log(jqXHR);  //응답 메시지
+                console.log(textStatus); //"error"로 고정인듯함
+                console.log(errorThrown);
+                console.log("실패");
+            }
+        })
+    }
+
+</script>
+
+
+
+   </script>
+
+
+
+
+</html>
+<?php /**PATH C:\xampp\htdocs\jolove2\laravel\resources\views/product.blade.php ENDPATH**/ ?>
